126244cc8f2f0587ea2a5223a63808c30a56b3dc
chenanguo <cag2050@163.com>
2019-11-26 16:28:02 +0800

    add: 依赖,配置,接口

--- src/main/java/com/example/alibabanacosdiscoveryserver/AlibabaNacosDiscoveryServerApplication.java
+++ src/main/java/com/example/alibabanacosdiscoveryserver/AlibabaNacosDiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package com.example.alibabanacosdiscoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AlibabaNacosDiscoveryServerApplication {
 
     public static void main(String[] args) {
